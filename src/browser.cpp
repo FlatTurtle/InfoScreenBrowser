e66346d33098a47fd8383e8b23bfd6aab17d3aa4
Tim Besard <tim.besard@gmail.com>
2011-06-04 17:23:56 +0200

    Removing unused files.

--- src/browser.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "browser.h"
-Browser::Browser() : QWebView() {
-}
-QString webPage::userAgentForUrl(const QUrl &url ) const {
-        return QString("test user agent");
-}
-webPage::webPage() : QWebPage() {
-}
