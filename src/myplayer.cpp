bd03e46d7d5ef9a78bf839c6ce99a01051f524ed
Johan Dams <jdmm@genesi-usa.com>
2013-08-05 17:35:07 +0300

    This should fix video lock ups. The issue is related to Qt somehow opening the file after every replay and probably not releasing resources properly. Instead of digging into the Qt code, work around it by setting the loaded video back to the beginning (rewind) and play again.

--- src/myplayer.cpp
+++ src/myplayer.cpp
@@ -11,11 +11,18 @@ MyPlayer::MyPlayer(const MyPlayer &): Phonon::VideoPlayer(0)
 }
 
 void MyPlayer::replay(void){
-	this->play(QUrl::fromLocalFile(current));
+    this->seek(0);
+    this->play();
+    //this->play(QUrl::fromLocalFile(current));
 }
 
 void MyPlayer::playfile(QString file){
-	qDebug() << "Playing: " << file;
-	current = file;
-	this->play(QUrl::fromLocalFile(current));
+    qDebug() << "Playing: " << file;
+    if(current != file){
+        current = file;
+        this->play(QUrl::fromLocalFile(current));
+    }else{
+        this->seek(0);
+        this->play();
+    }
 }
