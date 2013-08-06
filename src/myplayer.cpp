2ab58555f897d6a6836dc30f551bbfa8ac283539
Johan Dams <jdmm@genesi-usa.com>
2013-08-06 18:12:09 +0300

    Different approach to locking video...

--- src/myplayer.cpp
+++ src/myplayer.cpp
@@ -3,7 +3,6 @@
 
 MyPlayer::MyPlayer(QWidget *parent) : Phonon::VideoPlayer(Phonon::VideoCategory, parent)
 {
-    //connect(this, SIGNAL(finished()), this, SLOT(replay()));
 }
 
 MyPlayer::MyPlayer(const MyPlayer &): Phonon::VideoPlayer(0)
@@ -11,18 +10,13 @@ MyPlayer::MyPlayer(const MyPlayer &): Phonon::VideoPlayer(0)
 }
 
 void MyPlayer::replay(void){
-    this->seek(0);
-    this->play();
-    //this->play(QUrl::fromLocalFile(current));
+    this->mediaObject()->clear();
+    this->play(QUrl::fromLocalFile(current));
 }
 
 void MyPlayer::playfile(QString file){
     qDebug() << "Playing: " << file;
-    if(current != file){
-        current = file;
-        this->play(QUrl::fromLocalFile(current));
-    }else{
-        this->seek(0);
-        this->play();
-    }
+    current = file;
+    this->mediaObject()->clear();
+    this->play(QUrl::fromLocalFile(current));
 }
